23c9684bbb40baffc38c240680ab34ee96ed173d
JillianA328 <jpamelung@gmail.com>
2021-09-19 18:40:22 -0400

    working through errors

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,20 +2,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
-
 import { useDispatch, useSelector } from 'react-redux';
 import Cart from "../components/Cart";
-
-import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from '../utils/actions';
-
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS, } from '../utils/actions';
 import { idbPromise } from '../utils/helpers';
 
 function Detail() {
