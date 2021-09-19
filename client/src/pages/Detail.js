44312f80a1b93fc21307dd2862f05fec5ed24797
JillianA328 <jpamelung@gmail.com>
2021-09-18 23:14:29 -0400

    updated files for redux

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
-
+import { useDispatch, useSelector } from 'eact-redux'
 import Cart from "../components/Cart";
-import { useStoreContext } from "../utils/GlobalState";
+// import { useStoreContext } from "../utils/GlobalState";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,20 +15,22 @@ import { idbPromise } from "../utils/helpers";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state;
+  }
+  );
+
+  const dispatch = useDispatch();
   const { id } = useParams();
-
   const [currentProduct, setCurrentProduct] = useState({});
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-
   const { products, cart } = state;
 
   useEffect(() => {
     // already in global store
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    } 
+    }
     // retrieved from server
     else if (data) {
       dispatch({
@@ -103,8 +105,8 @@ function Detail() {
             <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button 
-              disabled={!cart.find(p => p._id === currentProduct._id)} 
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
               onClick={removeFromCart}
             >
               Remove from Cart
